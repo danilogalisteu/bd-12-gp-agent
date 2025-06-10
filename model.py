81d4ca0be721890f7edcbd8b16a3af67e3dc2e8f
Danilo Galisteu <danilogalisteu@gmail.com>
2025-06-10 10:46:33 -0300

    removed model code to separate file

---
+++ model.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+
+def get_model_response(user_prompt, model="gemini-2.0-flash-001"):
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model=model,
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
+    )
+    return (
+        response.text,
+        response.usage_metadata.prompt_token_count,
+        response.usage_metadata.candidates_token_count,
+    )
