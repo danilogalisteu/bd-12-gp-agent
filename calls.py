e662a42714e9d7f169c92b2d4d4d14624b163525
Danilo Galisteu <danilogalisteu@gmail.com>
2025-06-10 11:40:57 -0300

    code to call functions indicated by model

---
+++ calls.py
@@ -0,0 +1,43 @@
+from google.genai import types
+
+from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.run_python_file import run_python_file
+from functions.write_file import write_file
+
+
+functions_dict = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "run_python_file": run_python_file,
+    "write_file": write_file,
+}
+
+
+def call_function(function_call_part, working_directory, verbose=False):
+    if verbose:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        print(f" - Calling function: {function_call_part.name}")
+
+    if function_call_part.name not in functions_dict:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+
+    function_result = functions_dict[function_call_part.name](working_directory=working_directory, **function_call_part.args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_call_part.name,
+                response={"result": function_result},
+            )
+        ],
+    )
