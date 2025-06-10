a7101af2cd22ed60aa66f16baecb052aba042a19
Danilo Galisteu <danilogalisteu@gmail.com>
2025-06-10 11:14:09 -0300

    added schemas

--- schemas.py
+++ schemas.py
@@ -15,8 +15,56 @@ schema_get_files_info = types.FunctionDeclaration(
     ),
 )
 
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Read contents of the specified file into a string, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file path to read contents from, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Execute Python files with optional arguments.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The Pythno file path to execute, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Write or overwrite files, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file path to write or overwrite to, relative to the working directory.",
+            ),
+        },
+    ),
+)
+
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file,
     ]
 )
