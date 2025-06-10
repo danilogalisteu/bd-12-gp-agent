8ce49cef4967afb15deedbbe3d22b0b20fe9e6fd
Danilo Galisteu <danilogalisteu@gmail.com>
2025-06-10 10:57:55 -0300

    declaring available functions

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from schemas import available_functions
 
 
 load_dotenv()
@@ -11,7 +12,15 @@ client = genai.Client(api_key=api_key)
 
 
 def get_model_response(user_prompt, model="gemini-2.0-flash-001"):
-    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
@@ -19,8 +28,15 @@ def get_model_response(user_prompt, model="gemini-2.0-flash-001"):
     response = client.models.generate_content(
         model=model,
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=system_prompt),
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt,
+        ),
     )
+
+    for call in response.function_calls:
+        print(f"Calling function: {call.name}({call.args})")
+
     return (
         response.text,
         response.usage_metadata.prompt_token_count,
